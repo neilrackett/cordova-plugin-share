7e4968b66206826acb563193041a1ced34f93970
Neil Rackett <neil.rackett@mesmotronic.com>
2019-01-10 14:26:45 +0000

    Updated share

--- www/share.js
+++ www/share.js
@@ -1,13 +1,14 @@
-module.exports = function(text,title,mimetype,success,error){
-  if(typeof text !== "string") {
+module.exports = function(text, title, mimeType, success, error)
+{
+  if (typeof text !== "string") {
     text = "";
   }
-  if(typeof title !== "string") {
+  if (typeof title !== "string") {
     title = "Share";
   }
-  if(typeof mimetype !== "string") {
-    mimetype = "text/plain";
+  if (typeof mimeType !== "string") {
+    mimeType = "text/plain";
   }
-  cordova.exec(success,error,"Share","share",[text,title,mimetype]);
+  cordova.exec(success, error, "Share", "share", [text, title, mimeType]);
   return true;
 };
